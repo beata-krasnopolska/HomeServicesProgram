6c07616aebff212f577c346b55b8eed09db503bf
Beata-Krasnopolska <beata.krasnopolska@interia.pl>
2018-07-31 11:01:08 +0200

    Add project files.

---
+++ HomeServices/Job.cs
@@ -0,0 +1,73 @@
+﻿namespace HomeServices
+{
+    class Job
+    {
+        private string jobDescr;
+        private double time;
+        private double moneyRate;
+        private double totalFee;
+
+        public Job (string jobDescr, double time, double moneyRate)
+        {
+            JobDescr = jobDescr;
+            Time = time;
+            MoneyRate = moneyRate;            
+        }
+        public string JobDescr
+        {
+            get
+            {
+                return jobDescr;
+            }
+            set
+            {
+                jobDescr = value;
+            }
+        }
+        public double Time
+        {
+            get
+            {
+                return time;
+            }
+            set
+            {
+                time = value;
+                CalcTotalFee();
+            }
+        }
+        public double MoneyRate
+        {
+            get
+            {
+                return moneyRate;
+            }
+            set
+            {
+                moneyRate = value;
+                CalcTotalFee();
+            }
+        }
+        public double TotalFee
+        {
+            get
+            {
+                return totalFee;
+            }
+        }    
+        public static Job operator + (Job job1, Job job2)
+        {
+            string NewJobDescr = job1.JobDescr +" and " + job2.JobDescr;
+            double NewTime = job1.Time + job2.Time;
+            double NewMoneyRate = (job1.MoneyRate + job2.moneyRate)/2;
+            //double NewTotalFee = NewMoneyRate * NewTime;
+
+            Job newJob = new Job(NewJobDescr, NewTime, NewMoneyRate);
+            return newJob;
+        }
+        private void CalcTotalFee()
+        {
+            totalFee = moneyRate * time;
+        }
+    }
+}
---
+++ HomeServices/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HomeServices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Job job1 = new Job("wash windows", 3.5, 25);
+            Console.WriteLine("job description: {0}, planning time: {1}, money rate {2}, total fee {3}", job1.JobDescr, job1.Time, job1.MoneyRate, job1.TotalFee);
+            Job job2 = new Job("hoovering", 2.5, 30);
+            Console.WriteLine("job description: {0}, planning time: {1}, money rate {2}, total fee {3}", job2.JobDescr, job2.Time, job2.MoneyRate, job2.TotalFee);
+
+            Job job3 = job1 + job2;
+
+            Console.WriteLine("job description: {0}, planning time: {1}, money rate {2}, total fee {3}", job3.JobDescr, job3.Time, job3.MoneyRate, job3.TotalFee);
+            Console.ReadLine();
+        }
+    }
+}
